2e1a3a1d5b1d88b8c0954d4dbdc6d191a873f8fd
delightful-tree-6762 <mduckworth@gmail.com>
2019-03-01 06:34:34 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -15,3 +15,9 @@ def array_with_two_elements
     @taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
     my_first_element = @taylor_swift.first
   end
+
+
+  def third_element(my_third_element)
+   @taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
+   my_third_element = @taylor_swift[2]
+ end
