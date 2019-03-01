821490e9ba88648eb356c56185b7395ab481d565
delightful-tree-6762 <mduckworth@gmail.com>
2019-03-01 06:23:07 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -26,12 +26,12 @@ def last_element(my_last_element)
 
   def first_element_with_array_methods(first_country)
    @south_east_asia = ["Thailand", "Cambodia", "Singapore", "Myanmar"]
-   first_country = @south_east_asia.first
+   first_country = @south_east_asia.#first
  end
 
   def last_element_with_array_methods(last_country)
    @south_east_asia = ["Thailand", "Cambodia", "Singapore", "Myanmar"]
-   last_country = @south_east_asia.last
+   last_country = @south_east_asia.#last
  end
 
   def length_of_array(length)
