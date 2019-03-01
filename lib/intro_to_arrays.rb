0f0a3a9343652cbadcfd881dc2ac4ce1fd8c5704
delightful-tree-6762 <mduckworth@gmail.com>
2019-03-01 06:31:24 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -4,7 +4,8 @@ def instantiate_new_array
   @my_new_array = instantiate_new_array
 end
 
-define array_with_two_elements
-  array_with_two_elements = [1 , 200]
-  @my_new_array = array_with_two_elements
-end
+
+def array_with_two_elements
+   array_with_two_elements = [1, 2]
+   @my_two_array = array_with_two_elements
+ end
